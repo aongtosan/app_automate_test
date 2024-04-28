5a0101f42982fbce0fefe93baead8051e49f72f8
aongtosan <aongtosan@gmail.com>
2024-04-28 09:02:00 +0700

    update libs

--- Robots/poc/window_app_test.robot
+++ Robots/poc/window_app_test.robot
@@ -5,10 +5,4 @@ Library   ${EXECDIR}/libs/desktopAutomate.py
 Test Application
     ${test}     Test Custom Library
     Log To Console    ${test}  
-    # test_custom_libs
-    # Test Custom Library
-# Open Calculator
-#     Open Application    calc.exe
-#     ${window}    Set Active Window    Calculator
-#     ${result}    Click Button    7
-#     Should Be Equal As Integers    ${result}    7
+    Start Application
--- libs/desktopAutomate.py
+++ libs/desktopAutomate.py
@@ -1,9 +1,31 @@
 # import pywinauto
 from robot.api.deco import keyword
-# from pywinauto import application as pwa
+from pywinauto import Application 
 
 
 class desktopAutomate:
     @keyword("Test Custom Library")
     def test_custom_libs(self):
         return ("test access custom libs")
+    
+    @keyword("Open Commandline")
+    def open_cmd(self):
+        app = Application().start('cmd.exe')
+        cmd_dialog = app.window(title='Command Prompt')
+        return cmd_dialog
+    
+    @keyword("Execute Command")
+    def execute_command(self, app_dialog, command):
+        app_dialog.type_keys(command + "{ENTER}")
+
+    @keyword("Start Application")
+    def start_application(self) :
+        Application(backend='uia').start('..\\app_automate_test\\WebBinary\\win64\\chrome\\chrome.exe --force-renderer-accessibility')
+    
+    @keyword("Start Popup CMD")
+    def start_popup_cmd(self):
+        app = Application(backend='uia').start("cmd.exe")
+        time.sleep(2)  # Wait for 2 seconds for the window to appear
+        app_dialog = app.window(title="Command Prompt")
+        app_dialog.type_keys("minikube start" + "{ENTER}")
+        return app_dialog
